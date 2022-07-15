5caa7d5e9dbfdd884655eacb4c375df251e56d7d
AnastasiaR2 <mascheracapelli@gmail.com>
2022-07-15 20:19:31 +0300

    Complete first stage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,37 @@
 
-  let currency = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
-      currency = await currency.json();
+    let currencyList = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+        currencyList = await currencyList.json();
 
-  console.log(currency);
-  
-  let countries = await fetch('https://restcountries.com/v3.1/all');
-      countries = await countries.json();
+    let h1 = document.querySelector('h1');
 
-  console.log(countries);
+    h1.innerHTML = `Курси валют від НБУ на ${currencyList[0].exchangedate}`;
+
+    let countriesList = await fetch('https://restcountries.com/v3.1/all');
+        countriesList = await countriesList.json();
+
+    for(let i = 0; i < currencyList.length; i++){
+        currencyList[i].countries = [];
+        for(let j = 0; j < countriesList.length; j++){
+            for(let key in countriesList[j].currencies){
+                if(key == currencyList[i].cc){
+                    currencyList[i].countries.push(countriesList[j]);
+                }
+            }
+        }
+
+    }
+
+    console.log(currencyList);
+
+    elementsContainer.innerHTML = currencyList.map(item => `
+
+        <ul class="list-group list-group-horizontal mb-3">
+            <li class="list-group-item py-3 border-dark">${item.txt} (${item.cc})</li>
+            <li class="list-group-item py-3 border-dark">Курс: ${item.rate} грн.</li>
+            <li class="list-group-item py-3 border-dark"></li>
+        </ul>
+    `).join('');
+
+
+
+
