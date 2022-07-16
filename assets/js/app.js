acc10728e58f5000d5b27dad7f10bab3670bda59
AnastasiaR2 <mascheracapelli@gmail.com>
2022-07-16 20:34:46 +0300

    Add flags to currencies

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 
-    let currencyList = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
-        currencyList = await currencyList.json();
-
+    let date = new Date()
+    
     let h1 = document.querySelector('h1');
 
-    h1.innerHTML = `Курси валют від НБУ на ${currencyList[0].exchangedate}`;
+    h1.innerHTML = `Курси валют від НБУ на ${date.toLocaleDateString()}`;
+    
+    let currencyList = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+        currencyList = await currencyList.json();
 
     let countriesList = await fetch('https://restcountries.com/v3.1/all');
         countriesList = await countriesList.json();
@@ -23,15 +25,22 @@
 
     console.log(currencyList);
 
-    elementsContainer.innerHTML = currencyList.map(item => `
-
-        <ul class="list-group list-group-horizontal mb-3">
-            <li class="list-group-item py-3 border-dark">${item.txt} (${item.cc})</li>
-            <li class="list-group-item py-3 border-dark">Курс: ${item.rate} грн.</li>
-            <li class="list-group-item py-3 border-dark"></li>
-        </ul>
-    `).join('');
-
+    elementsContainer.innerHTML = currencyList.map(item => {
+        let flags = item.countries.map(country => `<img src="${country.flags.png}" alt="" class="border me-3 position-relative" style="width: 60px" onMouseOver="showTooltip()">`).join('');
+        return `
+        <div class="row row-col-3 align-items-center mb-3 p-2 border border-dark">
+            <div class="col">${item.txt} (${item.cc})</div>
+            <div class="col">Курс: ${item.rate} грн.</div>
+            <div class="col-8">${flags}</div>
+        </div>`
+    }).join('');
+    
+    document.querySelectorAll('img');
+    function showTooltip(img){
+        
+        img.innerHTML = test;
+    }
+    
 
 
 
